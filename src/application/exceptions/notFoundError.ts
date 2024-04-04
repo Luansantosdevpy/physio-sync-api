44b9917e20c31076c7f67adc41cbbc36957027f4
luansantosdevpy <luansantosdev.py@gmail.com>
2024-04-04 14:54:14 -0300

    feat: pattern exceptions

---
+++ src/application/exceptions/notFoundError.ts
@@ -0,0 +1,14 @@
+const buildErrorMessage = (msg: string, details?: unknown): string => {
+    return `${msg} ${details ? JSON.stringify(details) : ''}`.trim();
+  };
+  
+  export default class NotFoundError extends Error {
+    constructor(msg: string, details?: unknown) {
+      super(buildErrorMessage(msg, details));
+  
+      this.details = details;
+    }
+  
+    details: unknown;
+  }
+  
