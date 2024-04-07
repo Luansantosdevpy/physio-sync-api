0120550085e535932b2ce45b4466e951249f24cc
luansantosdevpy <luansantosdev.py@gmail.com>
2024-04-07 20:29:47 -0300

    feat: dependencies to category and client

--- src/dependencyContainer.ts
+++ src/dependencyContainer.ts
@@ -3,6 +3,12 @@ import Logger from './infrastructure/log/logger';
 import HealthCheckRepositoryInterface from './domain/interfaces/repositories/healthCheckRepositoryInterface';
 import PostgresHealthCheckRepository from './infrastructure/data/repositories/healthCheckRepository';
 import HealthCheckService from './application/services/healthCheckService';
+import CategoryRepositoryInterface from './domain/interfaces/repositories/categoryRepositoryInterface';
+import CategoryRepository from './infrastructure/data/repositories/categoryRepository';
+import CategoryService from './application/services/categoryService';
+import ClientRepositoryInterface from './domain/interfaces/repositories/clientRepositoryInterface';
+import ClientRepository from './infrastructure/data/repositories/clientRepository';
+import ClientService from './application/services/clientService';
 
 export default async (container: DependencyContainer): Promise<void> => {
   Logger.debug('Dependency container initializing...');
@@ -17,5 +23,28 @@ export default async (container: DependencyContainer): Promise<void> => {
   container.register<HealthCheckService>('HealthCheckService', {
     useClass: HealthCheckService
   });
+
+  container.register<CategoryRepositoryInterface>(
+    'CategoryRepositoryInterface',
+    {
+      useClass: CategoryRepository
+    }
+  );
+
+  container.register<CategoryService>('CategoryService', {
+    useClass: CategoryService
+  });
+
+  container.register<ClientRepositoryInterface>(
+    'ClientRepositoryInterface',
+    {
+      useClass: ClientRepository
+    }
+  );
+
+  container.register<ClientService>('ClientService', {
+    useClass: ClientService
+  });
+
   Logger.debug('Dependency container initialized!');
 };
