092ee5840a30cc9b9b9c54a97bf5c224a65a298b
luansantosdevpy <luansantosdev.py@gmail.com>
2024-04-07 20:29:28 -0300

    feat: add routes to client and category

--- src/api/routes/v1/v1.ts
+++ src/api/routes/v1/v1.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import healthCheck from './healthCheck';
 import swagger from './swagger';
+import category from './category';
+import client from './client';
 
 export default async (): Promise<Router> => {
   const router = Router();
 
   router.use('/api', router);
+  router.use('/category', await category());
+  router.use('/client', await client());
   router.use('/', await healthCheck());
   router.use('/', await swagger());
 
